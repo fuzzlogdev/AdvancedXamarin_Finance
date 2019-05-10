e85a9163a241f563eb2198cb81eac95b6b3ef073
Roger <fosterfreeze@gmail.com>
2019-05-09 21:16:32 -0700

    First Commit

---
+++ Finance/Finance/ViewModels/MainVM.cs
@@ -0,0 +1,46 @@
+﻿using Finance.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Finance.ViewModels
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public Posts Blog
+        {
+            get;
+            set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainVM()
+        {
+            ReadRSS();
+        }
+
+        public void ReadRSS()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Posts));
+
+            using (WebClient client = new WebClient())
+            {
+                string xml = Encoding.Default.GetString(client.DownloadData("https://www.finzen.mx/blog-feed.xml"));
+                using (Stream reader = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
+                {
+                    Blog = (Posts)serializer.Deserialize(reader);
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Finance/Finance/Views/PostPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using Finance.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Finance.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostPage : ContentPage
+    {
+        public PostPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+
+        public PostPage(Item item)
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+
+            webView.Source = item.ItemLink;
+        }
+    }
+}
