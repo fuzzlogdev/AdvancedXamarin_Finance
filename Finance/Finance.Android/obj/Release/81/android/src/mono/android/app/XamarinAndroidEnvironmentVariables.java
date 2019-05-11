960512ead504ab0e590859f73b60f24113120896
fuzzlogdev <fuzzlogdev@gmail.com>
2019-05-11 11:09:23 -0700

    Changed Android configuration to not "Use Shared Runtime"

---
+++ Finance/Finance.Android/obj/Release/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_DEBUG", "gen-compact-seq-points",
+		"XAMARIN_BUILD_ID", "22c2a8d8-32bf-4136-9d89-89bc1b0dd493",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
